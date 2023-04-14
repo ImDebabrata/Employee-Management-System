f0771fb43768c62387b63e216b883ce35f7a65e3
ImDebabrata <debabratakgt@hotmail.com>
2023-04-14 12:07:06 +0530

    added navbar

--- ems/src/App.jsx
+++ ems/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import "./App.css";
 import AddEmployee from "./pages/AddEmployee";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <AddEmployee />
     </div>
   );
---
+++ ems/src/components/Navbar.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Button,
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Box,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+
+const Navbar = () => {
+  const [openDrawer, setOpenDrawer] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpenDrawer(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpenDrawer(false);
+  };
+
+  return (
+    <Box flexGrow={1}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            margin="0 2 0 0"
+            color="inherit"
+            aria-label="menu"
+            onClick={handleDrawerOpen}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" flexGrow={1}>
+            Employee Management System
+          </Typography>
+          {/* <Button color="inherit">Login</Button> */}
+        </Toolbar>
+      </AppBar>
+      <Drawer open={openDrawer} onClose={handleDrawerClose}>
+        <Box
+          width={250}
+          role="presentation"
+          onClick={handleDrawerClose}
+          onKeyDown={handleDrawerClose}
+        >
+          <List>
+            <ListItem button>
+              <ListItemIcon>
+                <PersonAddIcon />
+              </ListItemIcon>
+              <ListItemText primary="Add User" />
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon>
+                <VisibilityIcon />
+              </ListItemIcon>
+              <ListItemText primary="View Users" />
+            </ListItem>
+          </List>
+        </Box>
+      </Drawer>
+    </Box>
+  );
+};
+
+export default Navbar;
