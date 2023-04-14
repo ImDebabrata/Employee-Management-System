e893410f42f2b6c990dd042795bc69b4cb33836a
ImDebabrata <debabratakgt@hotmail.com>
2023-04-14 13:37:30 +0530

    fix random users

--- backend/generateData.js
+++ backend/generateData.js
@@ -1,22 +1,36 @@
 const Mock = require("mockjs");
 const uuid = require("uuid");
 
-const mockUser = Mock.mock({
-  "users|50": [
-    {
+const database = { users: [] };
+
+const hobbiesList = [
+  "Reading",
+  "Music",
+  "Sports",
+  "Traveling",
+  "Cooking",
+  "Photography",
+  "Drawing",
+  "Writing",
+  "Dancing",
+  "Gardening",
+  "Gaming",
+  "Coding",
+];
+
+Mock.Random.shuffle(hobbiesList);
+
+for (let i = 0; i < 40; i++) {
+  database.users.push(
+    Mock.mock({
       id: () => uuid.v4(),
-      name: Mock.Random.name(),
-      email: Mock.Random.email(),
-      phone: Mock.Random.string("number", 10),
-      dob: Mock.Random.date(),
-      gender: Mock.Random.boolean() ? "male" : "female",
-      hobbies: Mock.Random.shuffle([
-        "reading",
-        "traveling",
-        "cooking",
-        "painting",
-      ]),
-    },
-  ],
-});
-console.log(JSON.stringify(mockUser));
+      name: "@name",
+      email: "@email",
+      phone: "@integer(10000000000,99999999999)",
+      dob: "@date",
+      gender: "@gender",
+      hobbies: Mock.Random.pick(hobbiesList, 3),
+    })
+  );
+}
+console.log(JSON.stringify(database));
